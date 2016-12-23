1b261027230eb212f94d48895af22318a98615b5
Pennan <1209036311@qq.com>
2016-12-23 16:02:38 +0800

    Update MainActivity.java

--- app/src/main/java/com/np/mygittest/MainActivity.java
+++ app/src/main/java/com/np/mygittest/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("TAG", "First update.");
+        Log.i("TAG", "I'm origin/master update.");
     }
 }
